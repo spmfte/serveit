38f6ab873fbc67769c202c68ca6b21c6cab257f7
Aidan Littman <35202874+spmfte@users.noreply.github.com>
2024-02-16 00:44:07 -0800

    Update runtime script

--- run_serveit.sh
+++ run_serveit.sh
@@ -3,9 +3,14 @@
 # Build the Rust project
 cargo build --release
 
-# Run the application with the specified directory
-./target/release/serveit "$1"
+# Run the application with the specified directory in the background
+./target/release/serveit "$1" &
+
+# Wait for the server to start
+sleep 2
 
 # Open the base URL in the default browser
 open "http://127.0.0.1:5001/files/"
 
+# Bring the Rust application back to the foreground (optional)
+fg

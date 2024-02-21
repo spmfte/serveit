3d4fd41814c0112ee39a6c7c866591d9d85532cb
Aidan Littman <35202874+spmfte@users.noreply.github.com>
2024-02-20 21:02:13 -0800

    Cargo clean

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::{App, Arg};
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
-use std::path::PathBuf;
+use std::{net::SocketAddr, path::PathBuf};
 use tokio::fs;
-use warp::{Filter, reject, Rejection, Reply};
+use warp::{http::Response, reject, Filter, Rejection, Reply};
 
 #[derive(Serialize, Deserialize)]
 struct DirEntry {
@@ -22,7 +21,7 @@ async fn main() {
             .required(true)
             .help("The path to the directory to serve"))
         .arg(Arg::with_name("port")
-            .short("p")
+            .short('p')
             .long("port")
             .takes_value(true)
             .default_value("3030")
@@ -33,23 +32,30 @@ async fn main() {
     let port: u16 = matches.value_of("port").unwrap().parse().expect("Port must be a number");
 
     let base_path = PathBuf::from(directory).canonicalize().unwrap();
-    let webui_path = base_path.join("webui/build"); // Assuming your React build folder is located here
+    let webui_path = base_path.join("webui/build");
     println!("Serving files from: {:?}", base_path);
     println!("Serving web UI from: {:?}", webui_path);
 
     let serve_dir = warp::path("files").and(warp::fs::dir(base_path.clone()));
     let serve_web_ui = warp::fs::dir(webui_path.clone());
+    let serve_index = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(webui_path.join("index.html")));
     let list_dir = warp::path("list")
         .and(warp::path::param())
         .and_then(move |path: String| {
             let base_path = base_path.clone();
             async move {
-                let full_path = base_path.join(path);
-                list_directory(full_path).await
+                list_directory(base_path.join(path)).await
             }
         });
 
-    let routes = serve_dir.or(list_dir).or(serve_web_ui);
+    let routes = serve_dir
+        .or(list_dir)
+        .or(serve_web_ui)
+        .or(serve_index)
+        .with(warp::cors().allow_any_origin())
+        .recover(handle_rejection);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("Listening on http://{}", addr);
@@ -58,7 +64,6 @@ async fn main() {
 
 async fn list_directory(path: PathBuf) -> Result<impl Reply, Rejection> {
     let mut entries = Vec::new();
-    // Declare read_dir as mutable
     let mut read_dir = fs::read_dir(path).await.map_err(|_| reject::not_found())?;
 
     while let Ok(Some(entry)) = read_dir.next_entry().await {
@@ -70,6 +75,21 @@ async fn list_directory(path: PathBuf) -> Result<impl Reply, Rejection> {
             is_dir: metadata.is_dir(),
         });
     }
-
     Ok(warp::reply::json(&entries))
 }
+
+async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    if err.is_not_found() {
+        Ok(warp::reply::with_status(
+            warp::reply::text("Not Found"),
+            warp::http::StatusCode::NOT_FOUND,
+        ))
+    } else {
+        eprintln!("handle_rejection - unhandled error: {:?}", err);
+        Ok(warp::reply::with_status(
+            warp::reply::text("Internal Server Error"),
+            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+        ))
+    }
+}
+

5dc33f4647f6197b40a7d1db598321fc95947b48
Aidan Littman <35202874+spmfte@users.noreply.github.com>
2024-02-24 01:07:58 -0800

    upadte app.js

--- serveit-dashboard/src/App.js
+++ serveit-dashboard/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IconButton } from '@material-tailwind/react';}
 import { SidebarWithSearch } from './Sidebar'; // Adjust the import path based on your file structure
 import './index.css'; // Assuming this imports Tailwind CSS
 
